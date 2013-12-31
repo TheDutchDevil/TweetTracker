0adb01deebd7d54035b0f67c7937e9dc5ad90796
dutchie169 <dutchie169@gmail.com>
2013-12-31 14:50:28 +0100

    getGraphColors now return a list with blue colors
    It's all blue and shiny

---
+++ Source/TweetTracker/ViewModels/Colors/ColorProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace TweetTracker.ViewModels.Colors
+{
+    class ColorProvider
+    {
+        public static List<Brush> getGraphColors(int amount)
+        {
+            List<Brush> graphColors = new List<Brush>();
+            for (int i = 0; i < amount; i++)
+            {             
+                graphColors.Add(Brushes.Blue);
+            }
+            return graphColors;
+        }
+    }
+}
