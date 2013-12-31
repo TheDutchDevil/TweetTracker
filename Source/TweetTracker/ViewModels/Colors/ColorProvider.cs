3e8282c537b0de1d8eb96aab30c8150b9c41c828
dutchie169 <dutchie169@gmail.com>
2013-12-31 15:14:13 +0100

    Added temporary switch statement
    ColorProvider can now handle up to seven, count 'em 7 different Colors!

--- Source/TweetTracker/ViewModels/Colors/ColorProvider.cs
+++ Source/TweetTracker/ViewModels/Colors/ColorProvider.cs
@@ -15,8 +15,31 @@ namespace TweetTracker.ViewModels.Colors
         public static Brush getNextColor()
         {
             Brush graphColor;
+            switch (providedColors.Count)
+            {
+                case 0:
+                    graphColor = Brushes.Blue;
+                    break;
+                case 1:
+                    graphColor = Brushes.Red;
+                    break;
+                case 2:
+                    graphColor = Brushes.HotPink;
+                    break;
+                case 3:
+                    graphColor = Brushes.Green;
+                    break;
+                case 4:
+                    graphColor = Brushes.Yellow;
+                    break;
+                case 5:
+                    graphColor = Brushes.Black;
+                    break;
+                default:
+                    graphColor = Brushes.BurlyWood;
+                    break;
+            }
             
-            graphColor = Brushes.Blue;
             providedColors.Add(graphColor);
             return graphColor;
         }
