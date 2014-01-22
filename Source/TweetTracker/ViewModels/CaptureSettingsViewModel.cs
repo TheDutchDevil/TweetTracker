2bd58574ec888b79855a946d417c89d4eee470b2
TheDutchDevil <nathancassee@gmail.com>
2014-01-22 18:26:41 +0100

    Implementation of a load and save button.

--- Source/TweetTracker/ViewModels/CaptureSettingsViewModel.cs
+++ Source/TweetTracker/ViewModels/CaptureSettingsViewModel.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using ServiceStack.Text;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +24,10 @@ namespace TweetTracker.ViewModels
             this._culture = string.Empty;
 
             this.AddCommand = new RelayCommand(() => this.SettingsRow.Add(new EditableSettingsRowViewModel(this.SettingsRow)));
+
+            this.SaveCommand = new RelayCommand(this.Save);
+
+            this.LoadCommand = new RelayCommand(this.Load);
         }
 
         public string Culture
@@ -59,13 +65,82 @@ namespace TweetTracker.ViewModels
             get
             {
                 return this._settingRows;
-            }            
+            }      
+            private set
+            {
+                this._settingRows = value;
+                this.OnPropertyChanged("SettingsRow");
+            }
         }
 
         public RelayCommand AddCommand
         {
             get;
             private set;
+        }
+
+        public RelayCommand SaveCommand
+        {
+            get;
+            private set;
+        }
+
+        public RelayCommand LoadCommand
+        {
+            get;
+            private set;
+        }
+
+        private void Save()
+        {
+            // Configure save file dialog box
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+            dlg.FileName = "Settings"; // Default file name
+            dlg.DefaultExt = ".set"; // Default file extension
+            dlg.Filter = "Settings (.set)|*.set"; // Filter files by extension
+
+            // Show save file dialog box
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process save file dialog box results
+            if (result == true)
+            {
+                // Save document
+                string filename = dlg.FileName;
+
+                var settings = TypeSerializer.SerializeToString(this);
+
+                File.WriteAllLines(filename, settings.Split('\n'));
+            }
+        }
+
+        private void Load()
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+
+
+            // Set filter for file extension and default file extension 
+            dlg.Filter = "Settings (*.set)|*.set";
+
+
+            // Display OpenFileDialog by calling ShowDialog method 
+            Nullable<bool> result = dlg.ShowDialog();
+
+
+            // Get the selected file name and display in a TextBox 
+            if (result == true)
+            {
+                // Open document 
+                string filename = dlg.FileName;
+                var contents = String.Join(string.Empty, File.ReadAllLines(filename));
+
+                CaptureSettingsViewModel model = TypeSerializer.DeserializeFromString<CaptureSettingsViewModel>(contents);
+
+                this.HashTag = model.HashTag;
+                this.Culture = model.Culture;
+                this.SettingsRow = model.SettingsRow;
+            }
         }
 
     }
